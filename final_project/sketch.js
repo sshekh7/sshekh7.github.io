388190a5e9567101b89ca824ac4f08c5067b68ab
sshekh7 <49168713+sshekh7@users.noreply.github.com>
2020-04-27 00:38:46 -0500

    Add files via upload

--- final_project/sketch.js
+++ final_project/sketch.js
@@ -37,12 +37,12 @@ class ScreenManager {
 
 class SoundManager {
   constructor() {
-    this.coronaDead = loadSound("../../assets/sounds/corona_dead.wav");
-    this.enemyDamage = loadSound("../../assets/sounds/enemy_damage.wav");
-    this.enemyShoot = loadSound("../../assets/sounds/enemy_shoot.wav");
-    this.playerDamage = loadSound("../../assets/sounds/player_damage.wav");
-    this.playerShoot = loadSound("../../assets/sounds/player_shoot.wav");
-    this.background = loadSound("../../assets/sounds/background.mp3");
+    this.coronaDead = loadSound("sounds/corona_dead.wav");
+    this.enemyDamage = loadSound("sounds/enemy_damage.wav");
+    this.enemyShoot = loadSound("sounds/enemy_shoot.wav");
+    this.playerDamage = loadSound("sounds/player_damage.wav");
+    this.playerShoot = loadSound("sounds/player_shoot.wav");
+    this.background = loadSound("sounds/background.mp3");
   }
 
   stopAllSounds() {
